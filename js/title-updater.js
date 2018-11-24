ab4a3430ce9260b67219056766e8caa9a842a62e
Reed Kavner <rik919@gmail.com>
2018-11-24 17:27:45 -0500

    numbers with commas

--- js/title-updater.js
+++ js/title-updater.js
@@ -1,9 +1,14 @@
 
 $(document).ready(function(){
+
+	function numberWithCommas(x) {
+    	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
+
     var n = 0;
 	var updater = setInterval(function(){ 
 	if (n < 1000000000){
-		document.title = n + " Bits";
+		document.title = numberWithCommas(n) + " Bits";
 		n = n + Math.floor(Math.random() * 999999);
 	}else{
 		document.title = "A Billion Bits";
